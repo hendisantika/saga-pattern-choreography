e8375e5e96b09f6ffa27dbe9eded156283d9d781
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 08:09:26 +0700

    #42 - Add CustomerOrder in Shipment Service 🚢🚚:octocat:✈️

---
+++ shipment/src/main/java/com/hendisantika/shipment/dto/CustomerOrder.java
@@ -0,0 +1,29 @@
+package com.hendisantika.shipment.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-choreography
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/13/2023
+ * Time: 8:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class CustomerOrder {
+    private String item;
+
+    private int quantity;
+
+    private double amount;
+
+    private String paymentMode;
+
+    private Long orderId;
+
+    private String address;
+}

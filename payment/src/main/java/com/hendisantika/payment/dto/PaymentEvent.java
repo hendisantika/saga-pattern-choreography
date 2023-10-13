ff62741492c958fc3157416385490d4912824c26
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 08:00:06 +0700

    #32 - Add PaymentEvent DTO in Payment Service 💸📗:octocat:💵

---
+++ payment/src/main/java/com/hendisantika/payment/dto/PaymentEvent.java
@@ -0,0 +1,21 @@
+package com.hendisantika.payment.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-choreography
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/13/2023
+ * Time: 7:59 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class PaymentEvent {
+    private String type;
+
+    private CustomerOrder order;
+}

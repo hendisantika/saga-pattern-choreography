953b19f415b493c0b0bb6c1b6cbef7b2639b428b
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 08:14:00 +0700

    #47 - Add shipOrder in ShipmentController in Shipment Service 🚢🚚:octocat:✈️

--- shipment/src/main/java/com/hendisantika/shipment/controller/ShipmentController.java
+++ shipment/src/main/java/com/hendisantika/shipment/controller/ShipmentController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.shipment.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.shipment.dto.CustomerOrder;
 import com.hendisantika.shipment.dto.InventoryEvent;
+import com.hendisantika.shipment.entity.Shipment;
 import com.hendisantika.shipment.repository.ShipmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -27,4 +33,33 @@ public class ShipmentController {
     private final ShipmentRepository shipmentRepository;
 
     private final KafkaTemplate<String, InventoryEvent> kafkaTemplate;
+
+    @KafkaListener(topics = "new-inventory", groupId = "inventory-group")
+    public void shipOrder(String event) throws JsonProcessingException {
+        Shipment shipment = new Shipment();
+        InventoryEvent inventoryEvent = new ObjectMapper().readValue(event, InventoryEvent.class);
+        CustomerOrder order = inventoryEvent.getOrder();
+        try {
+            if (order.getAddress() == null) {
+                throw new Exception("Address not present");
+            }
+            shipment.setAddress(order.getAddress());
+            shipment.setOrderId(order.getOrderId());
+            shipment.setStatus("success");
+            this.shipmentRepository.save(shipment);
+
+            // do other shipment logic ..
+
+        } catch (Exception e) {
+            shipment.setOrderId(order.getOrderId());
+            shipment.setStatus("failed");
+            this.shipmentRepository.save(shipment);
+
+            InventoryEvent reverseEvent = new InventoryEvent();
+            reverseEvent.setType("INVENTORY_REVERSED");
+            System.out.println(order);
+            reverseEvent.setOrder(order);
+            this.kafkaTemplate.send("reversed-inventory", reverseEvent);
+        }
+    }
 }

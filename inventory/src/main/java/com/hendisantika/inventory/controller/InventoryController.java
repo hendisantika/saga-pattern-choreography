81f23d0b9511c771153401966ca53ef63d390326
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 07:35:26 +0700

    #11 - Add updateInventory in InventoryController in Inventory Service ✏️📗:octocat:⬆

--- inventory/src/main/java/com/hendisantika/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/hendisantika/inventory/controller/InventoryController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.inventory.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.inventory.dto.CustomerOrder;
 import com.hendisantika.inventory.dto.InventoryEvent;
 import com.hendisantika.inventory.dto.PaymentEvent;
+import com.hendisantika.inventory.entity.Inventory;
 import com.hendisantika.inventory.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +35,44 @@ public class InventoryController {
     private final KafkaTemplate<String, InventoryEvent> kafkaTemplate;
 
     private final KafkaTemplate<String, PaymentEvent> kafkaPaymentTemplate;
+
+    @KafkaListener(topics = "new-payments", groupId = "payments-group")
+    public void updateInventory(String paymentEvent) throws JsonProcessingException {
+
+        InventoryEvent event = new InventoryEvent();
+
+        PaymentEvent p = new ObjectMapper().readValue(paymentEvent, PaymentEvent.class);
+        CustomerOrder order = p.getOrder();
+
+        try {
+
+            // update stock in database
+            Iterable<Inventory> inventories = this.inventoryRepository.findByItem(order.getItem());
+
+            boolean exists = inventories.iterator().hasNext();
+
+            if (!exists)
+                throw new Exception("Stock not available");
+
+            inventories.forEach(
+                    i -> {
+                        i.setQuantity(i.getQuantity() - order.getQuantity());
+
+                        this.inventoryRepository.save(i);
+                    });
+
+            event.setType("INVENTORY_UPDATED");
+            event.setOrder(p.getOrder());
+            this.kafkaTemplate.send("new-inventory", event);
+
+        } catch (Exception e) {
+
+            // reverse previous task
+            PaymentEvent pe = new PaymentEvent();
+            pe.setOrder(order);
+            pe.setType("PAYMENT_REVERSED");
+            this.kafkaPaymentTemplate.send("reversed-payments", pe);
+
+        }
+    }
 }

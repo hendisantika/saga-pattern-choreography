6e0656a5ac4c2feaf562d44d042aa5d54764adaa
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 07:31:27 +0700

    #6 - Add CustomerOrder DTO in Inventory Service ✏️📗:octocat:⬆

---
+++ inventory/src/main/java/com/hendisantika/inventory/dto/CustomerOrder.java
@@ -0,0 +1,30 @@
+package com.hendisantika.inventory.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-choreography
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/13/2023
+ * Time: 7:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class CustomerOrder {
+    private String item;
+
+    private int quantity;
+
+    private double amount;
+
+    private String paymentMode;
+
+    private Long orderId;
+
+    private String address;
+
+}

d403ae9e8a26d07c5a7949295281212c5e823128
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 08:05:09 +0700

    #38 - Add processPayment in PaymentController in Payment Service 💸📗:octocat:💵

--- payment/src/main/java/com/hendisantika/payment/controller/PaymentController.java
+++ payment/src/main/java/com/hendisantika/payment/controller/PaymentController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.payment.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.payment.dto.CustomerOrder;
 import com.hendisantika.payment.dto.OrderEvent;
 import com.hendisantika.payment.dto.PaymentEvent;
+import com.hendisantika.payment.entity.Payment;
 import com.hendisantika.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -29,4 +35,37 @@ public class PaymentController {
     private final KafkaTemplate<String, PaymentEvent> kafkaTemplate;
 
     private final KafkaTemplate<String, OrderEvent> kafkaOrderTemplate;
+
+    @KafkaListener(topics = "new-orders", groupId = "orders-group")
+    public void processPayment(String event) throws JsonProcessingException {
+        log.info("Recieved event" + event);
+        OrderEvent orderEvent = new ObjectMapper().readValue(event, OrderEvent.class);
+
+        CustomerOrder order = orderEvent.getOrder();
+        Payment payment = new Payment();
+        try {
+            // save payment details in db
+            payment.setAmount(order.getAmount());
+            payment.setMode(order.getPaymentMode());
+            payment.setOrderId(order.getOrderId());
+            payment.setStatus("SUCCESS");
+            this.paymentRepository.save(payment);
+
+            // publish payment created event for inventory microservice to consume.
+            PaymentEvent paymentEvent = new PaymentEvent();
+            paymentEvent.setOrder(orderEvent.getOrder());
+            paymentEvent.setType("PAYMENT_CREATED");
+            this.kafkaTemplate.send("new-payments", paymentEvent);
+        } catch (Exception e) {
+            payment.setOrderId(order.getOrderId());
+            payment.setStatus("FAILED");
+            paymentRepository.save(payment);
+
+            // reverse previous task
+            OrderEvent oe = new OrderEvent();
+            oe.setOrder(order);
+            oe.setType("ORDER_REVERSED");
+            this.kafkaOrderTemplate.send("reversed-orders", orderEvent);
+        }
+    }
 }

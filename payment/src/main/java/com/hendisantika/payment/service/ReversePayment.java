7cdbf769cc013848e0ab37aa7eac7b26abedffb6
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-13 08:03:17 +0700

    #36 - Add reversePayment in ReversePayment in Payment Service 💸📗:octocat:💵

--- payment/src/main/java/com/hendisantika/payment/service/ReversePayment.java
+++ payment/src/main/java/com/hendisantika/payment/service/ReversePayment.java
@@ -1,9 +1,14 @@
 package com.hendisantika.payment.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.payment.dto.CustomerOrder;
 import com.hendisantika.payment.dto.OrderEvent;
+import com.hendisantika.payment.dto.PaymentEvent;
+import com.hendisantika.payment.entity.Payment;
 import com.hendisantika.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -26,4 +31,29 @@ public class ReversePayment {
     private final PaymentRepository paymentRepository;
 
     private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    @KafkaListener(topics = "reversed-payments", groupId = "payments-group")
+    public void reversePayment(String event) {
+        try {
+            PaymentEvent paymentEvent = new ObjectMapper().readValue(event, PaymentEvent.class);
+
+            CustomerOrder order = paymentEvent.getOrder();
+
+            // do refund..
+            // update status as failed
+            Iterable<Payment> payments = this.paymentRepository.findByOrderId(order.getOrderId());
+            payments.forEach(p -> {
+                p.setStatus("FAILED");
+                this.paymentRepository.save(p);
+            });
+
+            // reverse previous task
+            OrderEvent orderEvent = new OrderEvent();
+            orderEvent.setOrder(paymentEvent.getOrder());
+            orderEvent.setType("ORDER_REVERSED");
+            this.kafkaTemplate.send("reversed-orders", orderEvent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
